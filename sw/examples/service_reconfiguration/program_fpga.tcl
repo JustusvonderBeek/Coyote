10168f95a21fd6c7546cfbad4159f292cd99060e
Justus <beek@in.tum.de>
2022-12-29 13:25:16 +0100

    Added flash fpga script

---
+++ sw/examples/service_reconfiguration/program_fpga.tcl
@@ -0,0 +1,15 @@
+open_hw_manager
+connect_hw_server -allow_non_jtag
+open_hw_target
+
+set Device [lindex [get_hw_devices] 0]
+current_hw_device $Device
+refresh_hw_device -update_hw_probes false $Device
+refresh_hw_device $Device
+set_property PROBES.FILE {} $Device
+set_property FULL_PROBES.FILE {} $Device
+set_property PROGRAM.FILE {$env(HOME)/Coyote_CSL/build_service_reconfiguration_hw/bitstreams/cyt_top.bit} $Device
+
+program_hw_devices $Device
+refresh_hw_device $Device
+exit
